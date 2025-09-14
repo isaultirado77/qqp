a59008a88a81168271ee34651709eeb48ef28d6e
Isaul <tiradolisaul@gmail.com>
2025-09-13 17:08:45 -0700

    init formating

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,7 @@
 """
 """
 
-from pathlib import Path
 
-
-BASE = Path().parent.absolute()
-DATA = BASE / "data"
-RAW = DATA / "raw"
-PROCESSED = DATA / "processed"
-
-DATA.mkdir(exist_ok=True)
-RAW.mkdir(exist_ok=True)
-PROCESSED.mkdir(exist_ok=True)
 
 if __name__ == '__main__':
     pass
